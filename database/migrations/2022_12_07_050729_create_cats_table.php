7b55f652668fee8773a95afc2f86763a87ea8508
adam waheed <adamwaheed@gmail.com>
2022-12-07 10:35:55 +0500

    Create Migration for Cat Masters

---
+++ database/migrations/2022_12_07_050729_create_cats_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cats', function (Blueprint $table) {
+            $table->id();
+            $table->string('bread_name',250);
+            $table->text('temperament')->nullable();
+            $table->string('lifespan');
+            $table->float('avg_weight_female');
+            $table->float('avg_weight_male');
+            $table->enum('coat_type',['non','short','medium','long'])->default('medium');
+            $table->enum('coat_density',['low','medium','high'])->default('medium');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cats');
+    }
+};
